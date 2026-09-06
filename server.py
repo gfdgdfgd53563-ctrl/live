719cbb2ade7b84d54441f06bd9868945b44c5ac4
gfdgdfgd53563-ctrl <gfdgdfgd53563@gmail.com>
2026-09-06 14:34:14 +0200

    Refactor server.py for improved socket handling

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
-import base64
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
-current_client = None
 latest_frame = None
 
 @app.route('/')
@@ -15,29 +14,21 @@ def index():
 
 @socketio.on('connect')
 def handle_connect():
-    global current_client
-    current_client = request.sid
-    emit('status', {'data': 'Connecté au serveur'})
-    if latest_frame:
-        emit('frame', {'image': latest_frame})
+    emit('status', {'connected': True})
 
 @socketio.on('send_frame')
 def handle_frame(data):
     global latest_frame
     latest_frame = data.get('image')
-    socketio.emit('frame', {'image': latest_frame})
+    socketio.emit('frame', {'image': latest_frame}, broadcast=True)
 
 @socketio.on('mouse_move')
-def handle_mouse_move(data):
-    socketio.emit('mouse_event', {'type': 'move', 'x': data['x'], 'y': data['y']}, to=current_client)
+def handle_mouse(data):
+    socketio.emit('mouse_event', data, broadcast=True)
 
 @socketio.on('mouse_click')
 def handle_click(data):
-    socketio.emit('mouse_event', {'type': 'click', 'button': data.get('button', 'left')}, to=current_client)
-
-@socketio.on('key_press')
-def handle_key(data):
-    socketio.emit('key_event', {'type': 'press', 'key': data['key']}, to=current_client)
+    socketio.emit('click_event', data, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
